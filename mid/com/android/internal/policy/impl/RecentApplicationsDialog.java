6a7300a790501587714ebcf69ac9f985f6b7acd7
Brian Crook <brian.w.crook@gmail.com>
2010-04-21 01:47:19 -0400

    Fixed missing ; on line 98

--- mid/com/android/internal/policy/impl/RecentApplicationsDialog.java
+++ mid/com/android/internal/policy/impl/RecentApplicationsDialog.java
@@ -97,7 +97,7 @@ public class RecentApplicationsDialog extends Dialog implements OnClickListener 
         mButtons[5] = findViewById(com.android.internal.R.id.button6);
         mButtons[6] = findViewById(com.android.internal.R.id.button7);
         mButtons[7] = findViewById(com.android.internal.R.id.button8);
-        mButtons[8] = findViewById(com.android.internal.R.id.button9)
+        mButtons[8] = findViewById(com.android.internal.R.id.button9);
         mNoAppsText = findViewById(com.android.internal.R.id.no_applications_message);
 
         for (View b : mButtons) {
